b745788aaca921c4bd9086bfd62ce8bca1756ee1
McIntoshY <McIntosh@McIntoshdeiMac.local>
2016-08-31 11:01:38 +0800

    fix can not open project

--- SJBaseOCProject/SJBaseOCProject/SJMacro/AppMacro.h
+++ SJBaseOCProject/SJBaseOCProject/SJMacro/AppMacro.h
@@ -11,24 +11,24 @@
 
 
 // 登录时需要传递的一些常量
-#define PTID            @"捷越贷款APP"
-#define CID             @"000000"
-#define LOGINKEY        @"JYLHAPP001"
-#define AESKey          @"abcdnnnnnn123456"
-#define APP_FOLDERKEY   @"JYLHAPPfky"
+#define PTID                         @"xx"
+#define CID                          @"xx"
+#define LOGINKEY                     @"JYLHAPP001"
+#define AESKey                       @"xx"
+#define APP_FOLDERKEY                @"xx"
 
 //folder path
-#define APP_DOCUMENT_DATA_IMAGE         @"images"
-#define APP_DOCUMENT_DATA_INTO          @"into_att"
-#define APP_DOCUMENT_DATA_THUMNAIL      @"thumbnails"
-#define APP_DOCUMENT_DATA_LOG           @"logs"
-#define APP_DOCUMENT_DATA_VOICE         @"voices"
-#define APP_DOCUMENT_DATA_LOCALVOICE    @"local_voices"
-#define APP_DOCUMENT_DATA_DB            @"db.sqlite"
-#define APP_DOCUMENT_DATA_DICTIONARY    @"dictionary_data"
+#define APP_DOCUMENT_DATA_IMAGE      @"images"
+#define APP_DOCUMENT_DATA_INTO       @"into_att"
+#define APP_DOCUMENT_DATA_THUMNAIL   @"thumbnails"
+#define APP_DOCUMENT_DATA_LOG        @"logs"
+#define APP_DOCUMENT_DATA_VOICE      @"voices"
+#define APP_DOCUMENT_DATA_LOCALVOICE @"local_voices"
+#define APP_DOCUMENT_DATA_DB         @"db.sqlite"
+#define APP_DOCUMENT_DATA_DICTIONARY @"dictionary_data"
 //---
 
-#define APP_UPDATE_VERSION  @"isNewVersionJY"
+#define APP_UPDATE_VERSION           @"isNewVersionJY"
 #define NAV_ICON_HEIGHT 27
 
 #define UPDATEAPP_ALERT_TAG  99
@@ -51,25 +51,27 @@
 
 
 // 缓存版本号
-#define APP_DataVersion     @"appDataVersion"
+#define APP_DataVersion    @"appDataVersion"
 //数据字典获取 证件类型的编码
-#define APP_IDTYPE          @"IDTYPE"
+#define APP_IDTYPE         @"IDTYPE"
 //数据字典获取 贷款产品的编码
-#define APP_LOAN_PRODUCT    @"C_CL_LOAN_PRODUCT"
+#define APP_LOAN_PRODUCT   @"C_CL_LOAN_PRODUCT"
 //数据字典获取 借款用途
-#define APP_LOANPURPOSE     @"LOANPURPOSE"
+#define APP_LOANPURPOSE    @"LOANPURPOSE"
 //数据字典获取 性别
-#define APP_SEX             @"SEX"
+#define APP_SEX            @"SEX"
 //数据字典获取 还款方式
-#define APP_REPAYMENTWAY    @"CM_REPAYMENTWAY"
+#define APP_REPAYMENTWAY   @"CM_REPAYMENTWAY"
 //数据字典获取 还款日
-#define APP_LOAN_DATE       @"DQ_LOAN_DATE_CALCULATION"
+#define APP_LOAN_DATE      @"DQ_LOAN_DATE_CALCULATION"
 //本地数据字典获取产品类型
-#define APP_LOCAL_PTYPE     @"LOCAL_attTypeList"
+#define APP_LOCAL_PTYPE    @"LOCAL_attTypeList"
 //放款机构字典
-#define APP_LOANFUNDS       @"LOANFUNDS"
+#define APP_LOANFUNDS      @"LOANFUNDS"
 //压缩图片大小
-#define APP_PNG_SIZE        @"APP_PNG_SIZE"
+#define APP_PNG_SIZE       @"APP_PNG_SIZE"
+
+#define APP_SELECT_TOUCHID @"APP_BOOL_TOUCHID"        //存储TouchID开关选项
 
 
 #define MAX_COUNT         18 // 选择图片最大数默认是9
@@ -80,7 +82,6 @@
 #define JYAPP_RequestTimeOut 10  //请求超时时间
 
 
-#define APP_SELECT_TOUCHID @"APP_BOOL_TOUCHID" //存储TouchID开关选项
 
 // 去除调用代理方法的警告
 #define SuppressPerformSelectorLeakWarning(Stuff) \
